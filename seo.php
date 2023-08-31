825ea490716ed635a950fdad83b527f0d36366f9
Titk484 <nguyentuankiet199@gmail.com>
2023-08-31 14:24:08 +0700

    first commit

---
+++ seo.php
@@ -0,0 +1,136 @@
+<?php 
+	include 'inc/header.php';
+	//include 'inc/slider.php';
+?>
+
+<style>
+body {font-family: "Times New Roman", Georgia, Serif;}
+h1, h2, h3,h4, h5, h6 {
+  font-family: "Playfair Display";
+  
+}
+ul.b {
+  list-style-type: square;
+  font-size: 25px;
+  padding-left: 50px;
+}
+
+span.a {
+  display: inline; /* the default for span */
+  width: 100px;
+  height: 100px;
+  color:red;
+  font-weight: bold;
+  /* background-color: lightgreen; */
+}
+</style>
+
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1600px;min-width:300px" id="home">
+  <img class="w3-image" src="img/12.jpg"  width="1600" height="400">
+  <div class="w3-display-bottomleft w3-padding-large w3-opacity">
+   
+  </div>
+</header>
+
+<!-- Page content -->
+<div class="w3-content" style="max-width:1100px">
+
+  <!-- About Section -->
+  <div class="w3-row w3-padding-60" id="about">
+    <div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="img/seo3.jpg" class="w3-round w3-image w3-opacity-min" style="width:200%" height="150">
+    </div>
+
+    <div class="w3-col m6 w3-padding-large" id="menu">
+      <h1 class="w3-center" style="font-weight:bold" >WinX Store</h1>
+      <h4 class="w3-center" style="font-weight:bold">THÀNH LẬP NĂM 2023</h4>
+      <p class="w3-large w3-text-grey w3-hide-medium">WinX Store là một cửa hàng linh kiện với quy mô nhỏ được thành lập bởi 4 Founders trẻ tuổi. Từng là những khách hàng với mong muốn có thể tìm được cho bản thân những sản phẩm phù hợp nhưng vẫn đáp ứng nhu cầu cơ bản chính hãng - giá cả hợp lý.
+          <br>Với niềm đam mê ấy đã tạo ra WinX Store của ngày hôm nay, WinX Store là thành quả của 3 năm không ngừng ước mơ -3 năm không ngừng cố gắng - 3 năm của sự miệt mài. </p> 
+          <!-- KHK Store được sinh ra với cả một câu chuyện dài từ sự khó khăn trong vấn đề tìm kiếm 
+          nguồn cung cấp sản phẩm, sự vất vả kêu gọi vốn từ các nhà đầu tư,...Nhưng những vấn đề
+          trên vẫn không khiến chúng tôi lùi bước. Với tất cả sự nhiệt thành KHK Store đã thành 
+          công được ra đời.
+          Vì những lý do trên, chúng tôi mong rằng với sự nhiệt thành trong khâu chuẩn bị cho KHK 
+          cũng như sự trân thành trong sự khảo sát-thu thập thông tin người dùng và mang đến những
+          sản phẩm tốt nhất. </p> -->
+    </div>
+  </div>
+  
+  <hr>
+  
+  <!-- Menu Section -->
+  <div class="w3-row w3-padding-50">
+    <div class="w3-col l6 w3-padding-large">
+      <h1 class="w3-center" style="font-family: Sofia, sans-serif; font-weight:bold">Best Sellers</h1><br>
+      <h4 >BÀN PHÍM LOGITECH G PRO KDA</h4>
+      <p class="w3-text-grey">Các phím switch cơ Tactile GX Brown,Chiếu sáng LIGHTSYNC RGB,...</p><br>
+    
+      <h4>BÀN PHÍM RAZER ORNATA EXPERT PIKACHU LIMITED EDITION</h4>
+      <p class="w3-text-grey">Bàn phím giả cơ với các phím có đèn nền riêng lẻ, ánh sáng màu vàng năng động 
+        của bàn phím có thể được tùy chỉnh thông qua Razer Synapse,...</p><br>
+    
+      <h4>BÀN PHÍM CƠ DAREU EK807G WIRELESS BROWN SWITCH</h4>
+      <p class="w3-text-grey">Chơi game, làm việc không gò bó về không gian, không sợ chập chờn đứt 
+        gãy dây cáp với bàn phím cơ DareU EK807G Wireless, kết nối không dây tốc độ cao.</p><br>
+    
+      <h4>TAI NGHE SONY MDR - ZX110AP</h4>
+      <p class="w3-text-grey">Tai nghe Sony MDR-ZX110AP được đánh giá khá cao từ thiết kế sang trọng cho tới chất âm cực đỉnh.</p><br>
+    
+      <h4>CHUỘT LOGITECH G PRO WIRELESS LEAGUE OF LEGENDS</h4>
+      <p class="w3-text-grey">Phối màu League Of Legends độc đáo, đây sẽ là dòng chuột gaming bạn không nên bỏ qua.</p>    
+    </div>
+    
+    <div class="w3-col l6 w3-padding-large">
+      <img src="img/seo5.jpg" class="w3-round w3-image w3-opacity-min" alt="Menu" style="width:100%">
+      <img src="img/seo6.jpg" class="w3-round w3-image w3-opacity-min" alt="Menu" style="width:100%">
+    </div>
+  </div>
+
+  <hr>
+
+
+  <div class="w3-row w3-padding-60" id="about">
+    <div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="img/seo2.png" class="w3-round w3-image w3-opacity-min" style="width:100%" height="90">
+    </div>
+
+    <div class="w3-col m6 w3-padding-large" id="menu">
+      <h1 class="w3-center" style="font-family: Sofia, sans-serif; font-weight:bold">Service</h1>
+      <h4 class="w3-center" style="font-weight:bold">TƯNG BỪNG KHAI TRƯƠNG - QUÀ TRAO LIỀN TAY</h4>
+      <h4 class="w3-center" style="font-weight:bold; color:red; font-size:20px"> 📢 Hàng ngàn chương trình khuyến mãi hấp dẫn 📢</h4>
+
+      <ul class="b">
+      <li >Sale sập sàn lên đến <span class="a">50%</span></li>
+      <li>Voucher chiết khấu <span class="a">50% cho mỗi hóa đơn</span> </li>
+      <li>Giao hàng miễn phí với hóa đơn <span class="a">từ 1tr</span></li>
+      <li>Giao hàng nhanh nội địa <span class="a">trong vòng 2h</span></li>
+      <li>Hỗ trợ<span class="a"> đổi trả 1-1 trong vòng 7 ngày </span></li>
+    </ul>
+
+    </div>
+  </div>
+
+  <!-- Contact Section
+  <div class="w3-container w3-padding-50" id="contact">
+    <h1>Liên hệ</h1><br>
+    <h4>Chúng tôi sẵn sàng hỗ trợ tư vấn mọi thắc mắc, đóng góp ý kiến từ phía khách hàng. Liên hệ nếu cần hỗ trợ.</h4>
+    <p class="w3-text-blue-grey w3-large"><b>Hỗ trợ hết mình - Ghi nhận những đánh giá từ khách hàng</b></p>
+    <p>Liên hệ với KHK Store qua số Hotline 1900 9999 hoặc  1900 7572 để có thể nhận được hỗ trợ một cách sớm nhất, hoặc có thể để lại thông tin liên lạc tại đây:</p>
+    <form action="/action_page.php" target="_blank">
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Tên" required name="Name"></p>
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Email hoặc SĐT" required name="Email"></p>
+      <p><input class="w3-input w3-padding-16" type="text" placeholder="Tin nhắn/đóng góp ý kiến từ khách hàng" required name="Message"></p>
+      <p><button class="w3-button w3-light-grey w3-section" type="submit">GỬI PHẢN HỒI</button></p>
+    </form>
+  </div> -->
+  
+<!-- End page content -->
+</div>
+
+</body>
+</html>
+
+<?php 
+	include 'inc/footer.php';
+?>
